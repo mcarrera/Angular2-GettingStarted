5bc626a23ce09644723ffae792e5cd657f76abcb
Marco <marco@farreachinc.com>
2017-03-12 14:46:50 -0500

    First Component

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -1,9 +1,22 @@
+
+
+
+
+
+// external functions or class, 3rd party, our exported stuff or modules from Angular2
 import { Component } from '@angular/core';
 
+// decorator (function) defines metadata
 @Component({
-    selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+    // properties
+    // directive name to be used in HTML
+    selector: 'pm-app', 
+    // view layout
+    template: '<div><h1>{{pageTitle}}</h1><div>First Component</div></div>'
 })
-export class AppComponent { }
+
+// class signature
+export class AppComponent {
+    pageTitle: string = 'ACME Product Management'
+
+ }
